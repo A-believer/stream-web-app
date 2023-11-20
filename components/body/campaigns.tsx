713de6292699f98df0485ade8271569d812583a9
David Abolade <69032300+A-believer@users.noreply.github.com>
2023-11-20 12:01:35 +0100

    update

--- components/body/campaigns.tsx
+++ components/body/campaigns.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import arrow from "../../public/icons/arrow-right-direction.svg"
 import arrowDark from "../../public/icons/arrow-right-direction-dark.svg"
 import arrowSmall from "../../public/icons/arrow-right-small.svg"
@@ -10,19 +10,32 @@ import { data } from '@/utils/data'
 import { useTheme } from 'next-themes'
 
 
+
 function Campaigns() {
-  const {theme} = useTheme()
+  const { theme } = useTheme()
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex + 1 === data.length ? 0 : prevIndex + 1
+    );
+  };
+  const handlePrevious = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex - 1 < 0 ? data.length - 1 : prevIndex - 1
+    );
+  };
   return (
     <section className='xl:px-[51px] md:px-6 px-5 xl:py-[100px] md:py-20 py-14 font-ppn w-full xl:space-y-[68px] space-y-[60px]'>
       <div className='flex md:justify-between justify-center'>
         <h3 className='md:text-5xl text-2xl md:leading-[70px] leading-10 font-semibold'>Completed Campaigns</h3>
         <div className='md:flex hidden gap-x-3 items-center '>
-          <Image src={theme === "light" ? arrow : arrowDark} alt='directions' className='rotate-180'/>
-          <Image src={theme === "light" ? arrow : arrowDark} alt='directions'/>
+          <Image src={theme === "light" ? arrow : arrowDark} alt='directions' className='rotate-180' onClick={handlePrevious}/>
+          <Image src={theme === "light" ? arrow : arrowDark} alt='directions' onClick={handleNext}/>
         </div>
       </div>
       
-      <div className='grid xl:grid-cols-3 md:grid-col-2 grid-cols-1 gap-x-[15px]'>
+      <div className='grid xl:grid-cols-3 md:grid-col-2 grid-cols-1 gap-x-[15px] overflow-hidden'>
        {
           data.map((items) => (
             <div key={items.id} className='border border-black/30 px-6 py-5 flex flex-col gap-y-6 w-full rounded-[20px]'>
@@ -41,8 +54,8 @@ function Campaigns() {
       </div>
 
       <div className='flex md:hidden gap-x-3 items-center justify-center'>
-          <Image src={theme === "light" ? arrow : arrowDark} alt='directions' className='rotate-180'/>
-          <Image src={theme === "light" ? arrow : arrowDark} alt='directions'/>
+          <Image src={theme === "light" ? arrow : arrowDark} alt='directions' className='rotate-180' onClick={handlePrevious}/>
+          <Image src={theme === "light" ? arrow : arrowDark} alt='directions' onClick={handleNext}/>
         </div>
 
     </section>
