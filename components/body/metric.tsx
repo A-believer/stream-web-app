9a1c9a18838cc46cc817477a35e654273e37a40c
David Abolade <69032300+A-believer@users.noreply.github.com>
2023-11-20 02:07:40 +0100

    faqs and testimonials complete

--- components/body/metric.tsx
+++ components/body/metric.tsx
@@ -1,8 +1,30 @@
+import Image from 'next/image'
 import React from 'react'
-
+import rocket from "../../public/images/rocket.png"
 function Metric() {
   return (
-    <div className='font-ppn'>Metric</div>
+    <section className='font-ppn text-foreground flex md:flex-row flex-col items-center justify-between xl:mx-20 md:mx-12 ml-5 mr-12 xl:my-20 md:my-14 my-8'>
+      <Image src={rocket} alt='rocket'  className='xl:w-[626px] xl:h-[626px] md:w-[317px] md:h-[317px] w-full h-full'/>
+
+      <div className='flex justify-center items-center  xl:gap-x-[160px] md:gap-x-[150px] gap-x-[80px] xl:gap-y-[77px] md:gap-y-[40px] gap-y-[35px] flex-wrap bg-[#138294]/5 xl:py-20 md:py-[71px] py-[60px] xl:px-10 md:px-6 rounded-[20px] relative'>
+        <div className='flex flex-col justify-center items-center gap-[15px]'>
+          <h3 className='xl:text-[60px] md:text-[30px] text-[27px]'>475K</h3>
+          <p className='xl:text-lg md:text-[10px] text-[8px] xl:leading-[70px] leading-[35px]'>Registered Helpers</p>
+        </div>
+
+        
+        <div className='flex flex-col justify-center items-center gap-[15px]'>
+          <h3 className='xl:text-[60px] md:text-[30px] text-[27px]'>105K</h3>
+          <p className='xl:text-lg md:text-[10px] text-[8px] xl:leading-[70px] leading-[35px]'>Projects Funded</p>
+        </div>
+
+        <div className='flex flex-col justify-center items-center gap-[15px]'>
+          <h3 className='xl:text-[60px] md:text-[30px] text-[27px]'>200K</h3>
+          <p className='xl:text-lg md:text-[10px] text-[8px] xl:leading-[70px] leading-[35px]'>Monthly Visitors</p>
+        </div>
+        <div className='absolute md:bottom-9 bottom-4 md:left-7 left-4 w-full h-full bg-transparent border-2 border-dashed border-[#138294] rounded-[20px]'></div>
+      </div>
+    </section>
   )
 }
 
--- components/body/testimonials.tsx
+++ components/body/testimonials.tsx
@@ -1,8 +1,48 @@
+/* eslint-disable react/no-unescaped-entities */
+import Image from 'next/image'
 import React from 'react'
+import t1 from "../../public/images/t1.png"
+import t2 from "../../public/images/t2.png"
+import t3 from "../../public/images/t3.png"
+import t4 from "../../public/images/t4.png"
+import t5 from "../../public/images/t5.png"
+import t6 from "../../public/images/t6.png"
+import t7 from "../../public/images/t7.png"
+import t8 from "../../public/images/t8.png"
+import t9 from "../../public/images/t9.png"
+import t10 from "../../public/images/t10.png"
+import t11 from "../../public/images/t11.png"
+import t12 from "../../public/images/t12.png"
+import stars from "../../public/icons/stars.svg"
 
 function Testimonials() {
   return (
-    <div>Testimonials</div>
+    <section className='font-ppn md:my-[140px] w-full'>
+      <h3 className='md:text-5xl text-2xl md:leading-[70px] leading-10 font-semibold text-center w-full'>Testimonials</h3>
+
+      <div className='flex justify-between items-center gap-x-16 mt-10'>
+        <div className='self-start'>
+          <p className='xl:w-[590px]  md:text-2xl text-lg md:leading-[60px] leading-[45px] font-medium'>"This platform is a Godsend! It's a portal for volunteers to connect with people in need. My family has been in critical health condition for almost a year but this platform made it so easy for me and connected me with people willing to help, as i am not so stable financially."</p>
+          <p className='text-[#138294] text-2xl mt-10 mb-4'>Mariam. C</p>
+          <Image src={stars} alt='stars'/>
+        </div>
+      <div className='xl:flex hidden flex-wrap gap-[5px]'>
+        <Image src={t1} alt='images'/>
+        <Image src={t2} alt='images'/>
+        <Image src={t3} alt='images'/>
+        <Image src={t4} alt='images'/>
+        <Image src={t5} alt='images'/>
+        <Image src={t6} alt='images'/>
+        <Image src={t7} alt='images'/>
+        <Image src={t8} alt='images'/>
+        <Image src={t9} alt='images'/>
+        <Image src={t10} alt='images'/>
+        <Image src={t11} alt='images'/>
+        <Image src={t12} alt='images'/>
+      </div>
+      </div>
+      
+    </section>
   )
 }
 
